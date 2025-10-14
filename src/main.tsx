58e20d066e9f8ba474b94062f2441ade09703156
eduardo <eduardomendes418@gmail.com>
2025-10-14 17:52:39 -0300

    [KB-04] feat - add main and App

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,30 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { Suspense } from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import { BrowserRouter } from "react-router-dom";
+import { StyleSheetManager } from "styled-components";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <Suspense fallback={<div>Loading...</div>}>
+    <BrowserRouter>
+      <StyleSheetManager shouldForwardProp={(prop) => prop !== "shake"}>
+        <App />
+        <ToastContainer
+          position="bottom-right"
+          autoClose={3000}
+          hideProgressBar={false}
+          newestOnTop={false}
+          closeOnClick
+          rtl={false}
+          pauseOnFocusLoss
+          draggable
+          pauseOnHover
+          theme="light"
+        />
+      </StyleSheetManager>
+    </BrowserRouter>
+  </Suspense>
+);
